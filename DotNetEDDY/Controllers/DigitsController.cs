bd3d309104c964d5fc9f4bb30d98328799c21565
Douglas Hernández <douglas.hf@alliedglobal.com>
2024-06-05 12:50:03 -0600

    adding digit controller and removing some unused imports

---
+++ DotNetEDDY/Controllers/DigitsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetEDDY.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DigitsController : ControllerBase
+    {
+        private static int[] data = Enumerable.Range(1,100).ToArray();
+
+        [HttpGet]
+        public IActionResult Get() => Ok(data);
+    }
+}
