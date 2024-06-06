66235a6bee7b579d8f9fb94969cf4ff8c857e933
Douglas Hernández <douglas.hf@alliedglobal.com>
2024-06-05 22:01:52 -0600

    adding the digits functionality

---
+++ DotNetEDDY/Controllers/DigitsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetEDDY.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DigitsController : ControllerBase
+    {
+        private static List<int> data = Enumerable.Range(1, 100).ToList();
+
+        [HttpGet]
+        public IActionResult Get() => Ok(new { count = data.Count, data });
+
+        [HttpGet("test")]
+        public IActionResult Post()
+        {
+            List<int> toBeRemoved = GetRandom();
+
+            data = data.Where(x => !toBeRemoved.Contains(x)).ToList();
+
+
+            return Ok(new { coutn = data.Count, removed = toBeRemoved, data});
+        }
+
+        private List<int> GetRandom()
+        {
+            Random numbers = new Random(Guid.NewGuid().GetHashCode());
+            int minor = data.Order().FirstOrDefault();
+            int major = data.OrderByDescending(x => x).FirstOrDefault();
+
+            return new List<int>()
+            {
+                numbers.Next(minor, major),
+                numbers.Next(minor, major),
+                numbers.Next(minor, major)
+            };
+        }
+    }
+}
